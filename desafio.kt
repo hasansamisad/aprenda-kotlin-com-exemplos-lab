9465626456c66335eea044ab1d6e68e1b4385762
Hasan Sami Sad <hasan.trabalho@gmail.com>
2025-12-04 13:11:52 -0300

    Refinando desafio.kt

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,74 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { 
+    BASICO, 
+    INTERMEDIARIO, 
+    AVANCADO 
+}
 
-class Usuario
+data class Usuario(val nome: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class ConteudoEducacional(
+    val nome: String, 
+    val duracao: Int, 
+    val nivel: Nivel 
+) 
 
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) { 
+    
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        if (!inscritos.contains(usuario)) {
+            inscritos.add(usuario)
+            println("${usuario.nome} matriculado(a) na formação '$nome'.")
+        } else {
+            println("${usuario.nome} já está matriculado(a) nesta formação.")
+        }
+    }
+    
+    fun calcularDuracaoTotal(): Int {
+        return conteudos.sumOf { it.duracao }
+    }
+    
+    fun listarInscritos() {
+        println("\n--- Inscritos na Formação '$nome' (${inscritos.size} total) ---")
+        if (inscritos.isEmpty()) {
+            println("Ninguém está inscrito ainda.")
+        } else {
+            inscritos.forEachIndexed { index, usuario ->
+                println("${index + 1}. ${usuario.nome}")
+            }
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    
+    val disciplinaIA : ConteudoEducacional = ConteudoEducacional("Introdução à IA", 30, Nivel.BASICO)
+    val disciplinaCloud : ConteudoEducacional = ConteudoEducacional("Cloud Computing Essencial", 40, Nivel.INTERMEDIARIO)
+    val disciplinaKotlin : ConteudoEducacional = ConteudoEducacional("Kotlin Avançado", 50, Nivel.AVANCADO)
+
+    val listaConteudoKotlinIA = listOf(disciplinaKotlin, disciplinaIA, disciplinaCloud)
+
+    val kotlinComIA = Formacao(
+        nome = "Kotlin Com IA na Cloud",
+        conteudos = listaConteudoKotlinIA
+    )
+    
+    val hasan = Usuario("Hasan")
+    val daniel = Usuario("Daniel")
+    val vinicius = Usuario("Vinicius")
+    
+    kotlinComIA.matricular(hasan)
+    kotlinComIA.matricular(daniel)
+    kotlinComIA.matricular(vinicius)
+    kotlinComIA.matricular(hasan) 
+
+    println("\nNome da Formação: ${kotlinComIA.nome}")
+    println("Total de Conteúdos: ${kotlinComIA.conteudos.size}")
+    println("Duração Total: ${kotlinComIA.calcularDuracaoTotal()} minutos") 
+    
+    kotlinComIA.listarInscritos()
 }
